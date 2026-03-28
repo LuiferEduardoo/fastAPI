61cafb39a618718d3387cb5ba358ae3b9503c667
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-03-28 15:54:53 -0500

    feat: add the properties as a database field

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,15 +1,15 @@
-from pydantic import BaseModel, EmailStr
-from sqlModel import SQLModel
+from pydantic import  EmailStr
+from sqlModel import SQLModel, Field
 
 
 class CustomerBase(SQLModel):
-    name: str
-    description: str | None
-    email: EmailStr
-    age: int
+    name: str = Field(default=None)
+    description: str | None = Field(default=None)
+    email: EmailStr = Field(default=None)
+    age: int = Field(default=None)
 
 class CustomerCreate(CustomerBase):
     pass
 
 class Customer(CustomerBase, table=True):
-    id: int | None
+    id: int | None = Field(default=None, primary_key=True)

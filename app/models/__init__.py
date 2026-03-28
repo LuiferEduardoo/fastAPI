dc2ed0be7a9267b82ef45e0b5a7845a6ea53c46d
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-03-28 10:20:52 -0500

    feat: import models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,9 @@
 # Modelos de la aplicación
 from .customer import Customer
+from .customer import CustomerCreate
 from .transaction import Transaction
+from .transaction import TransactionCreate
 from .invoice import Invoice
+from .invoice import InvoiceCreate
 
-__all__ = ["Customer", "Transaction", "Invoice"]
+__all__ = ["Customer", "CustomerCreate", "Transaction", "TransactionCreate", "Invoice", "InvoiceCreate"]

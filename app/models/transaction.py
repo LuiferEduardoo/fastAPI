b451bbbb2de74d301c16363b19dcc89f7544d007
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-03-28 15:58:10 -0500

    feat: add the properties as a database field

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -1,12 +1,12 @@
-from sqlModel import SQLModel
+from sqlModel import SQLModel, Field
 
 
 class TransactionBase(SQLModel):
-    amount: int
-    description: str
+    amount: int = Field(default=None)
+    description: str = Field(default=None)
 
 class TransactionCreate(TransactionBase):
     pass
 
 class Transaction(TransactionBase, table=True):
-    id: int | None
+    id: int | None = Field(default=None, primary_key=True)

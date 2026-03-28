a753533dc8de23ddb3940816119515f933228ada
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-03-28 10:16:55 -0500

    feat: improve model customer

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,8 +2,14 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 
-class Customer(BaseModel):
+class CustomerBase(BaseModel):
     name: str
-    description: str
+    description: str | None
     email: EmailStr
     age: int
+
+class CustomerCreate(CustomerBase):
+    pass
+
+class Customer(CustomerBase):
+    id: int | None
